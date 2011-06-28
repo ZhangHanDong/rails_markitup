2244fb21f81d6b69a12238f63e930e40c2de7f24
AlexZhang <blackanger.z@gmail.com>
2011-06-28 11:37:58 +0800

    modify

--- lib/generators/rails_markitup/install_generator.rb
+++ lib/generators/rails_markitup/install_generator.rb
@@ -1,3 +1,4 @@
+require 'rake'
 require 'rails/generators'
 
 # ==================================
@@ -12,16 +13,15 @@ module RailsMarkitup
       source_root File.expand_path('../media', __FILE__)
       desc "Copies media files to main project"
       def copy_media_files
-        @images = Dir.glob("../media/images/*")
+        path = File.expand_path('../media', __FILE__)
+        @images = FileList["#{path}/images/*"]
         @images.each do |image|
-          copy_file image, "public/stylesheets/markitup/image/#{image.split('/').last}"
+          copy_file "#{image}", "public/stylesheets/markitup/image/#{image.split('/').last}"
         end
-        
         copy_file "../media/javascripts/jquery.markitup.js",       "public/javascripts/markitup/jquery.markitup.js"
         copy_file "../media/javascripts/sets/markdown/set.js",     "public/javascripts/markitup/sets/markdown/set.js"
         copy_file "../media/stylesheets/sets/markdown/style.css",  "public/stylesheets/markitup/sets/markdown/style.css"
         copy_file "../media/stylesheets/skins/markitup/style.css", "public/stylesheets/markitup/skins/markdown/style.css"
-
       end
     end
 
--- lib/rails_markitup/engine.rb
+++ lib/rails_markitup/engine.rb
@@ -8,8 +8,8 @@ module RailsMarkitup
     # = = stylesheet_link_tag    :markitup =
     # =====================================
     config.before_initialize do
-      config.action_view.javascript_expansions[:markitup] = %w()
-      config.action_view.stylesheet_expansions[:markitup] = %w()
+      config.action_view.javascript_expansions[:markitup] = %w(markitup/jquery.markitup.js markitup/sets/markdown/set.js)
+      config.action_view.stylesheet_expansions[:markitup] = %w(markitup/sets/markdown/style.css markitup/skins/markdown/style.css)
     end
     
   end
