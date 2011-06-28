f4036739a1fb94d68418f6ca10627fb7999a6adf
AlexZhang <blackanger.z@gmail.com>
2011-06-28 10:51:27 +0800

    modify

--- lib/generators/rails_markitup/install_generator.rb
+++ lib/generators/rails_markitup/install_generator.rb
@@ -1,8 +1,8 @@
 require 'rails/generators'
 
-# =================================
+# ==================================
 # = rails g rails_markitup:install =
-# =================================
+# ==================================
 
 module RailsMarkitup
   module Generators
@@ -12,7 +12,11 @@ module RailsMarkitup
       source_root File.expand_path('../media', __FILE__)
       desc "Copies media files to main project"
       def copy_media_files
-        copy_file "../media/images/*.png",                         "public/stylesheets/markitup/image/"
+        @images = Dir.glob("../media/images/*")
+        @images.each do |image|
+          copy_file image, "public/stylesheets/markitup/image/#{image.split('/').last}"
+        end
+        
         copy_file "../media/javascripts/jquery.markitup.js",       "public/javascripts/markitup/jquery.markitup.js"
         copy_file "../media/javascripts/sets/markdown/set.js",     "public/javascripts/markitup/sets/markdown/set.js"
         copy_file "../media/stylesheets/sets/markdown/style.css",  "public/stylesheets/markitup/sets/markdown/style.css"
